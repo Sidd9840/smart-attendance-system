c81cccb826ab8f4cefdb8bf03de561a724a6aa4c
Sidd9840 <siddu17022002@gmail.com>
2026-06-10 22:51:41 +0530

    Update report.js

--- JS/report.js
+++ JS/report.js
@@ -1,88 +1,15 @@
-function generateReport()
-{
-    let type = document.getElementById("reportType").value;
-    let date = document.getElementById("reportDate").value;
-    let month = document.getElementById("reportMonth").value;
+let attendance =
+JSON.parse(localStorage.getItem("attendance")) || [];
 
-    let attendance =
-    JSON.parse(localStorage.getItem("attendance")) || [];
+let table =
+document.getElementById("reportTable");
 
-    let table = document.getElementById("reportTable");
+attendance.forEach(function(record){
 
-    table.innerHTML = ` 
-        <tr>
-            <th>Roll No</th>
-            <th>Total Classes</th>
-            <th>Present</th>
-            <th>Attendance %</th>
-            <th>Status</th>
-        </tr>
-    `;
+    let row = table.insertRow();
 
-    let filtered = attendance.filter(function(a)
-    {
-        if(type === "daily")
-        {
-            return a.date === date;
-        }
-        else
-        {
-            return a.date.startsWith(month);
-        }
-    });
+    row.insertCell(0).innerHTML = record.name;
+    row.insertCell(1).innerHTML = record.roll;
+    row.insertCell(2).innerHTML = record.status;
 
-    let report = {};
-
-    filtered.forEach(function(a)
-    {
-        if(!report[a.roll])
-        {
-            report[a.roll] = {
-                total: 0,
-                present: 0
-            };
-        }
-
-        report[a.roll].total++;
-
-        if(a.status === "Present")
-        {
-            report[a.roll].present++;
-        }
-    });
-
-    if(Object.keys(report).length === 0)
-    {
-        table.innerHTML += `
-            <tr>
-                <td colspan="5">No attendance record found</td>
-            </tr>
-        `;
-        return;
-    }
-
-    for(let roll in report)
-    {
-        let total = report[roll].total;
-        let present = report[roll].present;
-        let percentage = Math.round((present / total) * 100);
-
-        let status =
-        percentage < 75 ? "Low Attendance" : "Good";
-
-        let color =
-        percentage < 75 ? "red" : "green";
-
-        table.innerHTML += `
-            <tr>
-                <td>${roll}</td>
-                <td>${total}</td>
-                <td>${present}</td>
-                <td>${percentage}%</td>
-                <td style="color:${color}; font-weight:bold;">
-                    ${status}
-                </td>
-            </tr>
-        `;
-    }
-}
+});
